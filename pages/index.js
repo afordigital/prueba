e77bf4993db5234919d41275348068a70bbb3d05
Aforina <43246362+Aforina@users.noreply.github.com>
2021-12-27 15:36:44 +0100

    testing

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home () {
           <div class='flex flex-wrap flex-col lg:w-1/2 p-20 lg:float-left'>
             <h2 class='mb-5 text-5xl sm:justify-left lg:text-3xl xl:text-5xl xl:mb-4 lg:mb-2 font-bold tracking-wider'>CONTACT FORM</h2>
             <h2 class='text-justify text-md sm:text-lg md:text-xl lg:text-base xl:text-2xl 2xl:text-xl mb-6'>Now that you know a lot about me, let me know if you are interested to work with me.</h2>
-            <img class='items-center lg:items-start rounded-lg' src='/maps-alessio.jpg' alt='maps' />
+            <img class='items-center lg:items-start rounded-lg' src='/maps-alessio.JPG' alt='maps' />
           </div>
           <div class='flex flex-wrap flex-col tracking-wider lg:w-1/2 lg:float-right pt-10 lg:pt-32 pl-16 md:pl-12 pr-20 text-md sm:text-lg md:text-xl lg:text-base xl:text-2xl 2xl:text-xl'>
             <div class='text-left font-bold inline-flex'>
