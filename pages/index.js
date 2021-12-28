b627df7ee1ecbcc719d18247b0f4e2266ce7c33a
Aforina <43246362+Aforina@users.noreply.github.com>
2021-12-28 16:23:37 +0100

    fixing bugs

--- pages/index.js
+++ pages/index.js
@@ -86,16 +86,16 @@ export default function Home () {
           </div>
         </section>
         <div>
-          <footer class='container bg-gray-300 w-screen flex flex-col tracking-wider sm:flex-row text-xl font-semibold items:center pb-6 pt-12'>
-            <div class='flex flex-wrap mx-auto items-center justify-center m-1 md:justify-center gap-12 cursor-pointer'>
+          <footer class='bottom-0 inset-x-0 bg-gray-300 w-screen flex flex-col tracking-wider sm:flex-row text-xl font-semibold items:center pb-6 pt-12'>
+            <div class='container flex flex-wrap mx-auto items-center justify-center m-1 md:justify-center gap-12 cursor-pointer'>
               <p>Alessio Muganni</p>
             </div>
-            <div class='flex flex-wrap mx-auto flex-col md:flex-row items-center justify-center md:justify-center md:gap-12 gap-4 m-4 cursor-pointer'>
+            <div class='container flex flex-wrap mx-auto flex-col md:flex-row items-center justify-center md:justify-center md:gap-12 gap-4 m-4 cursor-pointer'>
               <p>Home</p>
               <p>About me</p>
               <p>Contact</p>
             </div>
-            <div class='flex flex-wrap mx-auto mt-2 md:mt-0 items-center justify-center md:justify-center gap-12 cursor-pointer'>
+            <div class='container flex flex-wrap mx-auto mt-2 md:mt-0 items-center justify-center md:justify-center gap-12 cursor-pointer'>
               <p><FaFacebookSquare /> </p>
               <p><FaLinkedin /> </p>
               <p><FaInstagram /> </p>
