877d153aa9e278b2123a995e94535fd4df544496
Aforina <43246362+Aforina@users.noreply.github.com>
2022-01-28 21:07:54 +0100

    contact
    upgrades with contact and design

--- components/About.js
+++ components/About.js
@@ -5,15 +5,28 @@ const About = () => {
     <div id='about' className='w-full max-w-7xl mx-auto p-4 pt-20'>
       <div className='w-full grid grid-cols-2'>
         <div className='col-span-2 md:col-span-1 relative min-h-[200px] md:min-h-none'>
-          <img className='rounded-lg' src='/alessio.jpg' alt='Alessio Muganni' />
+          <img
+            className='rounded-lg'
+            src='/alessio.jpg'
+            alt='Alessio Muganni'
+          />
         </div>
         <div className='col-span-2 md:col-span-1 p-10'>
-          <h2 className='mb-5 text-5xl justify-left lg:text-3xl xl:text-5xl xl:mb-4 lg:mb-2 font-semibold tracking-wider text-gray-800'>ABOUT ME</h2>
+          <h2 className='mb-5 text-5xl justify-left lg:text-3xl xl:text-5xl xl:mb-4 lg:mb-2 font-semibold tracking-wider text-gray-800'>
+            ABOUT ME
+          </h2>
           <div>
-            <h3 className='text-justify text-md sm:text-lg md:text-xl lg:text-base xl:text-xl 2xl:text-1xl mb-6 text-gray-800'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </h3>
+            <h3 className='text-justify text-md sm:text-lg md:text-xl lg:text-base xl:text-xl 2xl:text-1xl mb-6 text-gray-800'>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.{' '}
+            </h3>
             <nav className='text-center p-3 m-5 justify-center md:text-4 lg:text-base xl:text-xl cursor-pointer bg-blue-500 hover:bg-blue-700 text-gray-50 rounded-full'>
               <Link href='/timeline'>VIEW MY PORTFOLIO</Link>
-            </nav>º
+            </nav>
           </div>
         </div>
       </div>
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,51 +6,13 @@ import {
   FaCheckSquare
 } from 'react-icons/fa'
 // import Link from 'next/link'
-import React from 'react'
-import { Formik } from 'formik'
+import React, { useState } from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 const ContactForm = () => {
+  const [submittedForm, changeSubmittedForm] = useState(false)
   return (
     <>
-      <Formik
-        initialValues={{
-          nombre: '',
-          email: ''
-        }}
-        onSubmit={() => {
-          console.log('Formulario enviado')
-        }}
-      >
-        {({ values, handleSubmit, handleChange, handleBlur }) => (
-          <form className='formulario' onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor='nombre'>Nombre</label>
-              <input
-                type='text'
-                id='nombre'
-                name='nombre'
-                placeholder='name'
-                value={values.nombre}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </div>
-            <div>
-              <label htmlFor='email'>Email</label>
-              <input
-                type='text'
-                id='email'
-                name='email'
-                placeholder='name@email.com'
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </div>
-            <button type='submit'>Enviar</button>
-          </form>
-        )}
-      </Formik>
       <div id='contact' className='w-full max-w-7xl mx-auto p-4 pt-20 pb-20'>
         <div>
           <h2 className='text-5xl lg:text-3xl xl:text-5xl font-semibold tracking-wider'>
@@ -66,26 +28,161 @@ const ContactForm = () => {
             </div>
             <div className='col-span-2 md:col-span-1'>
               <div className='tracking-wider text-gray-800 pl-16 md:pl-12 pr-20 text-md sm:text-lg md:text-xl lg:text-base xl:text-2xl 2xl:text-xl'>
-                <div className='text-left font-semibold inline-flex'>
-                  <FaUser className='mt-1' />
-                  <p className='pl-3'>Name</p>
-                </div>
-                <div className='bg-blue-100 w-full h-14 rounded-lg mt-3 mb-4 shadow-lg' />
-                <div className='text-left font-semibold inline-flex'>
-                  <FaEnvelope className='mt-1' />
-                  <p className='pl-3'>Email</p>
-                </div>
-                <div className='bg-blue-100 w-full h-14 rounded-lg mt-3 mb-4 shadow-lg' />
-                <div className='text-left font-semibold inline-flex'>
-                  <FaPhoneAlt className='mt-1' />
-                  <p className='pl-3'>Phone (Optional)</p>
-                </div>
-                <div className='bg-blue-100 w-full h-14 rounded-lg mt-3 mb-4 shadow-lg' />
-                <div className='text-left font-semibold inline-flex'>
-                  <FaCommentAlt className='mt-1' />
-                  <p className='pl-3'>Message</p>
-                </div>
-                <div className='bg-blue-100 w-full h-24 rounded-lg mt-3 mb-4 shadow-lg' />
+                <Formik
+                  initialValues={{
+                    name: '',
+                    email: '',
+                    number: '',
+                    message: ''
+                  }}
+                  validate={valores => {
+                    const errores = {}
+
+                    // Name Validation
+                    if (!valores.name) {
+                      errores.name = 'Por favor ingresa un name'
+                    } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name)) {
+                      errores.name =
+                        'El name solo puede contener letras y espacios'
+                    }
+
+                    // Email Validation
+                    if (!valores.email) {
+                      errores.email = 'Por favor ingresa un email'
+                    } else if (
+                      !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+                        valores.email
+                      )
+                    ) {
+                      errores.email =
+                        'El email solo puede contener letras, números, puntos, guiones y guión bajo'
+                    }
+
+                    // Number Validation
+                    if (!/^[0-9]{2,3}-? ?[0-9]{6,7}$/.test(valores.number)) {
+                      errores.number = 'El número no es válido'
+                    }
+
+                    // Message Validation
+                    if (!valores.message) {
+                      errores.message = 'Por favor escribe un mensaje'
+                    }
+
+                    return errores
+                  }}
+                  onSubmit={(valores, { resetForm }) => {
+                    resetForm()
+                    changeSubmittedForm(true)
+                    setTimeout(() => changeSubmittedForm(false), 5000)
+                  }}
+                >
+                  {({ errors }) => (
+                    <Form className='formulario'>
+                      <div>
+                        <div class='inline-flex'>
+                          <FaUser className='mt-1 mr-2' />
+                          <label
+                            htmlFor='name'
+                            className='text-left font-semibold inline-flex'
+                          >
+                            Name
+                          </label>
+                        </div>
+                        <div className='bg-blue-100 w-full h-14 rounded-lg mt-3 mb-4 shadow-lg'>
+                          <Field
+                            type='text'
+                            id='name'
+                            name='name'
+                            placeholder='Name'
+                            className='decoration-none bg-blue-100 rounded-lg pl-3 pt-1'
+                          />
+                        </div>
+                        <ErrorMessage
+                          name='name'
+                          component={() => (
+                            <div className='error'>{errors.name}</div>
+                          )}
+                        />
+                      </div>
+                      <div>
+                        <div class='inline-flex'>
+                          <FaEnvelope className='mt-1.5 mr-2' />
+                          <label
+                            htmlFor='email'
+                            className='text-left font-semibold inline-flex'
+                          >
+                            Email
+                          </label>
+                        </div>
+                        <Field
+                          type='text'
+                          id='email'
+                          name='email'
+                          placeholder='Email Address'
+                          className='bg-blue-100 w-full h-14 rounded-lg mt-3 mb-4 shadow-lg'
+                        />
+                        <ErrorMessage
+                          name='email'
+                          component={() => (
+                            <div className='error'>{errors.email}</div>
+                          )}
+                        />
+                      </div>
+                      <div>
+                        <div class='inline-flex'>
+                          <FaPhoneAlt className='mt-1 mr-2' />
+                          <label
+                            htmlFor='number'
+                            className='text-left font-semibold inline-flex'
+                          >
+                            Number
+                          </label>
+                        </div>
+                        <Field
+                          type='text'
+                          id='number'
+                          name='number'
+                          placeholder='Phone'
+                          className='bg-blue-100 w-full h-14 rounded-lg mt-3 mb-4 shadow-lg'
+                        />
+                        <ErrorMessage
+                          name='number'
+                          component={() => (
+                            <div className='error'>{errors.number}</div>
+                          )}
+                        />
+                      </div>
+                      <div>
+                        <div class='inline-flex'>
+                          <FaCommentAlt className='mt-1.5 mr-2' />
+                          <label
+                            htmlFor='message'
+                            className='text-left font-semibold inline-flex'
+                          >
+                            Message
+                          </label>
+                        </div>
+                        <Field
+                          name='message'
+                          as='textarea'
+                          placeholder='Message'
+                          className='bg-blue-100 w-full h-24 rounded-lg mt-3 mb-4 shadow-lg'
+                        />
+                        <ErrorMessage
+                          name='message'
+                          component={() => (
+                            <div className='error'>{errors.message}</div>
+                          )}
+                        />
+                      </div>
+                      <button type='submit'>Enviar</button>
+
+                      {submittedForm && (
+                        <p className='exito'>Formulario enviado con éxito!</p>
+                      )}
+                    </Form>
+                  )}
+                </Formik>
                 <div className='justify-center text-center'>
                   <div className='font-semibold inline-flex'>
                     <FaCheckSquare className='mt-1' />
