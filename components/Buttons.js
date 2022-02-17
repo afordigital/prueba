050cf8feb9e53ffcdf0f6c624beae6a3b6899b6b
Hogas Andrei Marius <hogasandreimarius@gmail.com>
2022-02-17 20:06:21 +0100

    ++ useRef ++Validation ++ Yup

---
+++ components/Buttons.js
@@ -0,0 +1,7 @@
+export const Buttons = () => {
+  return <div> normal button</div>
+}
+
+export const midButton = () => {
+  return <div> Mid button </div>
+}
--- components/Map.js
+++ components/Map.js
@@ -3,10 +3,12 @@ import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
 
 const Map = props => {
   return (
-    <GoogleMap
-      defaultZoom={16}
-      defaultCenter={{ lat: 41.900749, lng: 12.483367 }}
-    />
+    <div>
+      <GoogleMap
+        defaultZoom={16}
+        defaultCenter={{ lat: 41.900749, lng: 12.483367 }}
+      />
+    </div>
   )
 }
 
